124722f3639dc7f7150f6c8a2dde02e5f290093f
Mateusz Wilczek <36714554+mateuszwilczek@users.noreply.github.com>
2018-03-25 23:18:48 +0200

    Start preliminary data analysis

--- merge.R
+++ merge.R
@@ -440,3 +440,45 @@ GetAparcValue <- function(sub_id, hemi, struct, meas){
     return(A[match(sub_id, A$SUB_ID),
              match(paste0(struct, "_", hemi, "_", meas), names(A))])
 }
+
+
+#### analysis ####
+# sort by pairNumber, exclude not matched
+B <- A[order(A$pairNumber, A$pairClass), ]
+B <- B[B$matched, ]
+
+# verify proper sorting
+all(B$pairNumber[B$pairClass == "control"] ==
+    B$pairNumber[B$pairClass == "case"])
+
+# case-vs-control wrappers for wilcox.test and t.test
+wilcox.cvc <- function(x) {
+    return(wilcox.test(x[B$pairClass == "case"],
+                       x[B$pairClass == "control"],
+                       paired = TRUE))
+}
+t.cvc <- function(x) {
+    return(t.test(x[B$pairClass == "case"],
+                  x[B$pairClass == "control"],
+                  paired = TRUE))
+}
+
+# compare WhiteSurfArea
+WhiteSurfArea_total <- B$WhiteSurfArea_rh + B$WhiteSurfArea_lh
+wilcox.cvc(WhiteSurfArea_total)
+wilcox.cvc(B$WhiteSurfArea_rh)
+wilcox.cvc(B$WhiteSurfArea_lh)
+
+# compare WhiteSurfArea realtive to CC_area
+wilcox.cvc(WhiteSurfArea_total / B$CC_area)
+wilcox.cvc(B$WhiteSurfArea_rh / B$CC_area)
+wilcox.cvc(B$WhiteSurfArea_lh / B$CC_area)
+
+# compare MeanThickness
+wilcox.cvc(B$MeanThickness_rh)
+wilcox.cvc(B$MeanThickness_lh)
+
+# compare TotalGrayVol
+wilcox.cvc(B$TotalGrayVol)
+wilcox.cvc(B$TotalGrayVol / B$CC_area)
+
