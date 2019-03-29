5c5581d7093b5cc566d909d89edc61019dcf9c51
Mateusz Wilczek <36714554+mateuszwilczek@users.noreply.github.com>
2019-03-29 17:28:03 +0100

    Update download.sh
    Now can be just run without modifications to download all necessary files

--- data/pcp/download.sh
+++ data/pcp/download.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-# https://s3.amazonaws.com/fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/$FILE_ID/$SUBDIR/$OUTPUT_FILE
-
+# aseg.stats
 FILE_NAME=aseg.stats
+
+mkdir $FILE_NAME
 
 while IFS= read -r FILE_ID
 do
-    wget https://s3.amazonaws.com/fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/$FILE_ID/stats/$FILE_NAME -O out/$FILE_ID.$FILE_NAME
+    wget https://s3.amazonaws.com/fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/$FILE_ID/stats/$FILE_NAME -O $FILE_NAME/$FILE_ID.$FILE_NAME
+done < FILE_ID.txt
+
+
+# lh.aparc.stats
+FILE_NAME=lh.aparc.stats
+
+mkdir $FILE_NAME
+
+while IFS= read -r FILE_ID
+do
+    wget https://s3.amazonaws.com/fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/$FILE_ID/stats/$FILE_NAME -O $FILE_NAME/$FILE_ID.$FILE_NAME
+done < FILE_ID.txt
+
+
+# rh.aparc.stats
+FILE_NAME=rh.aparc.stats
+
+mkdir $FILE_NAME
+
+while IFS= read -r FILE_ID
+do
+    wget https://s3.amazonaws.com/fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/$FILE_ID/stats/$FILE_NAME -O $FILE_NAME/$FILE_ID.$FILE_NAME
 done < FILE_ID.txt
